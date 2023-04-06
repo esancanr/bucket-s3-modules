d369500f18b801737e14e47074d9eeebe2a505d7
Edison <128564946+esancanr@users.noreply.github.com>
2023-04-06 22:49:13 +0200

    Update main.tf

--- modules/main.tf
+++ modules/main.tf
@@ -1,7 +1,7 @@
 #Creation of the S3 bucket
 resource "aws_s3_bucket" "bucket" {
     bucket = var.name_bucket
-    acl = var.visibility
+    acl = var.acl
 
     tags = {
     Name        = var.name_tag
